cbf07ca9c20d6a39fc1824d12ccbc8c1865df329
JavierLainez420 <101200751+JavierLainez420@users.noreply.github.com>
2022-05-06 11:14:10 -0600

    Create Eje3

--- demo_crud/src/demo_crud2/Eje2.java
+++ demo_crud/src/demo_crud2/Eje2.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package demo_crud2;
 
-/**
- *
- * @author Marihuano
- */
+
 public class Eje2 {
      public static void main(String[] args) {
         double cantidadPrestada, tiempo, tasaDeIntereses;
